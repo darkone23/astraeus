7c17dbb0c1fb56bf6ee0cd8d5be6f4ec514b15f4
Thomas Omans <thomas.omans@gmail.com>
2014-03-21 13:48:36 -0700

    hooking up mocha tests :coffee:

--- src/lib/inventories.js
+++ src/lib/inventories.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var fs = require('fs'),
+var fs = require('q-io/fs'),
     _ = require("mori"),
     q = require("q"),
     modeToPermissions = require('mode-to-permissions');
 
 function isExecuteable(stats) {
-    return modeToPermissions(stats.mode).execute.owner;
+    return modeToPermissions(stats.node.mode).execute.owner;
 };
 
 function readInventoryScript(path) {
@@ -20,12 +20,13 @@ function readInventoryIni(path) {
 function readInventory(path) {
     return function(stats) {
         return isExecuteable(stats) ?
-            readInventoryScript(path) : readInventoryIni(path);
+            readInventoryScript(path) :
+            readInventoryIni(path);
     };
 };
 
 function getInventory(path) {
-    return q.denodeify(fs.stat)(path).then(readInventory(path));
+    return fs.stat(path).then(readInventory(path));
 };
 
 function getInventories(inventoryPaths) {
--- test/test.js
+++ test/test.js
@@ -1,9 +1,15 @@
-var assert = require("assert");
+var inventories = require("../src/lib/inventories.js"),
+    chai = require("chai"),
+    should = chai.should(),
+    promises = chai.use(require("chai-as-promised"));
 
 describe('ansible inventories', function() {
-  describe('loading from disk', function(){
-    it('should run executeable inventory scripts', function(){
-      assert.equal(-1, 1);
+    describe('loading from disk', function() {
+        it('should run executeable inventory scripts', function() {
+            return inventories.load("test/fixtures/hosts.sh").should.eventually.equal("script");
+        });
+        it('should parse non-executeable inventories as ini', function() {
+            return inventories.load("test/fixtures/hosts.ini").should.eventually.equal("ini");
+        });
     });
-  });
 });
