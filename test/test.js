5f141b68197447a1257e865112916f79aa86d784
Thomas Omans <thomas.omans@gmail.com>
2014-03-20 20:07:35 -0700

    big bang :cyclone:

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe('ansible inventories', function() {
+  describe('loading from disk', function(){
+    it('should run executeable inventory scripts', function(){
+      assert.equal(-1, 1);
+    });
+  });
+});
