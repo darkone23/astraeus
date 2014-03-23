15366a06bec7a57cd20db9c7dfe000a2886f928e
Thomas Omans <thomas.omans@gmail.com>
2014-03-23 15:42:26 -0700

    using restify to serve static :wine_glass:

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-var inventories = require("./lib/inventories.js")
+var inventories = require("./lib/inventories.js"),
+    restify = require('restify'),
+    server;
+
+server = restify.createServer();
+
+server.get(/.*/, restify.serveStatic({
+  directory: './public',
+  default: 'index.html'
+}));
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
