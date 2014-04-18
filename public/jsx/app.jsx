bac9c5b0db17e45432d98e98b4ad48b9d8fabf92
Thomas Omans <thomas.omans@gmail.com>
2014-04-18 15:14:28 -0700

    initial helpers for working with inventory data structure :zap:

--- public/jsx/app.jsx
+++ public/jsx/app.jsx
@@ -2,8 +2,40 @@
 
 define(["lodash", "react"], function(_, React) {
 
+    function concat(a, b) {
+        return a.concat(b);
+    }
+
+    function mapcat(coll, f, _this) {
+        return _
+            .map(coll, f, _this)
+            .reduce(concat, []);
+    }
+
+    function getHosts(inventory) {
+        var hosts =  mapcat(inventory, function(val) {
+            return _.isArray(val.hosts) ? val.hosts : [];
+        });
+        return _.uniq(hosts).sort();
+    }
+
+    function Host(host) {
+        return <li> { host } </li>;
+    }
+
     function Inventory(inventory) {
-        return <span> { inventory.path } </span>;
+        var hosts = getHosts(inventory.inventory),
+            hosts = _.map(hosts, Host, this);
+        return (
+        <div>
+            <span>
+              { inventory.path }
+            </span>
+            <ul>
+              { hosts }
+            </ul>
+        </div>
+        );
     }
 
     return React.createClass({
